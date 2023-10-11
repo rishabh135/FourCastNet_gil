43e845fbcca5befacd9927264ee614a3d6720994
Rishabh Gupta <gupt1075@gilbreth-fe00.rcac.purdue.edu>
2023-10-11 19:12:49 -0400

    removed oreography flag to correct the shape issue, works now for inference

--- submit_gilbreth.sh
+++ submit_gilbreth.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 # change the directory path of model run-time output and error messages to your own
-#SBATCH --output=/scratch/gilbreth/gupt1075/run_infer_fourcastnet.out
-#SBATCH --error=/scratch/gilbreth/gupt1075/run_infer_fourcastnet.err
+#SBATCH --output=/scratch/gilbreth/gupt1075/run_infer_fourcastnet_oct.out
+#SBATCH --error=/scratch/gilbreth/gupt1075/run_infer_fourcastnet_oct.err
 # The file name of this submission file, so it's easier to track jobs
 # filename: submit_run_model_example.sub
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=32
 #SBATCH --gres=gpu:1 
-#SBATCH --time=23:00:00
+#SBATCH --time=1:00:00
 # partner queue has a 24-hour limit
 #SBATCH -A gdsp-k
-#SBATCH -C  "v100|a100"
+#SBATCH -C  "v100|a100|a30"
 # Job name, it will show up when you track this job
 #SBATCH -J fourcastnet_job
 # Use your email address so that you will receive email notifications about the job begin, end, or fail status
@@ -32,7 +32,8 @@ export CUDA="11.7"
 
 echo $PYTHONPATH
 
-
+echo "$now"
+echo "Current date completed loading modules: $now"
 
 # track per-code GPU load
 monitor gpu percent --all-cores >gpu-percent.log &
@@ -64,10 +65,10 @@ conda activate pytorch
 # cd /scratch/gilbreth/wwtung/FourCastNet/
 
 python /scratch/gilbreth/gupt1075/FourCastNet/inference/inference.py \
-       --config=afno_backbone \
-       --run_num=0 \
-       --weights '/scratch/gilbreth/gupt1075/model_weights/FCN_weights_v0/backbone.ckpt' \
-       --override_dir '/scratch/gilbreth/gupt1075/ERA5_expts_gtc/' 
+       --config='afno_backbone' \
+       --run_num='02' \
+       --weights "/scratch/gilbreth/gupt1075/model_weights/FCN_weights_v0/backbone.ckpt"  \
+       --override_dir '/scratch/gilbreth/gupt1075/ERA5_expts_gtc_2/' 
 
 
 
