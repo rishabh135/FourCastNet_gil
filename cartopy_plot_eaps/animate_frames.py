7dc82a032b238e3aa2afecd89ba89d3459e87e7d
Rishabh Gupta <gupt1075@gilbreth-fe00.rcac.purdue.edu>
2023-12-15 15:46:05 -0500

    changing datetime format

--- cartopy_plot_eaps/animate_frames.py
+++ cartopy_plot_eaps/animate_frames.py
@@ -6,16 +6,16 @@ from PIL import Image
 frames = []
 imgs = sorted(glob.glob("./KHGX_3/ref_KHGX_2017*.png"))
 for i in imgs:
-    print(f" curretnly working on {i}  ")
+    print(f" currently working on {i}  ")
     new_frame = Image.open(i)
     frames.append(new_frame)
 
 # Save into a GIF file that loops forever
 frames[0].save(
-    "./hurricane_harvey_hourly.gif",
+    "./hurricane_harvey_hourly_60x.gif",
     format="GIF",
     append_images=frames[1:],
     save_all=True,
-    duration=6000,
+    duration=100,
     loop=0,
 )
