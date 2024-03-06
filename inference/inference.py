df2bdc99d4cfd15b0e361c4b03587443b3fdc673
Rishabh Gupta <gupt1075@gilbreth-k011.rcac.purdue.edu>
2024-03-06 11:31:33 -0500

    removed extra logging from the autoregressive step

--- inference/inference.py
+++ inference/inference.py
@@ -57,6 +57,20 @@ sys.path.append(os.path.dirname(os.path.realpath(__file__)) + '/../')
 import logging
 from collections import OrderedDict
 
+
+
+
+import sys
+username="gupt1075"
+
+
+if (f"/home/{username}/FourCastNet_gil" not in sys.path):
+    sys.path.append(f"/home/{username}/FourCastNet_gil")
+
+# sys.path.append(os.path.dirname(os.path.realpath(__file__)) + '/../')
+sys.path.append(f"/home/{username}/FourCastNet_gil")
+
+
 import h5py
 import torch
 import torch.cuda.amp as amp
